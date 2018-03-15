1fee208e7aad194620379d8d62ad8a74ab241fd0
Ricardo <saint1039@gmail.com>
2018-03-15 13:03:08 -0600

    Se agrego la clase scannerViewController

---
+++ ScannerViewController.swift
@@ -0,0 +1,175 @@
+//
+//  ViewController.swift
+//  scaner
+//
+//  Created by ine on 07/11/16.
+//  Copyright © 2016 saint. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+protocol TransferirTexto: class {
+    
+    func transferir(texto : String)
+    
+}
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var qrCodeFrameView: UIView?
+    
+    var barCode: String!
+    
+    var delegate : TransferirTexto?
+    
+    let supportedBarCodes = [AVMetadataObjectTypeQRCode, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeUPCECode, AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeAztecCode]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Cancelar", for: .normal)
+        button.addTarget(self, action: #selector(dismis), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.green
+        button.layer.cornerRadius = 5
+        button.clipsToBounds = true
+        
+        
+        let videoCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed();
+            return;
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = supportedBarCodes
+        } else {
+            failed()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession);
+        previewLayer.frame = view.layer.bounds;
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        
+        
+        view.layer.addSublayer(previewLayer);
+        view.addSubview(button)
+        
+        captureSession.startRunning();
+        
+        
+        button.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant : -8).isActive = true
+        button.topAnchor.constraint(equalTo: self.view.topAnchor,constant: 20).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        qrCodeFrameView = UIView()
+        
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
+        }
+    }
+    
+    func dismis(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func failed() {
+        let ac = UIAlertController(title: "Escaneo no soportado", message: "Su dispositivo no admite la exploración de un código desde un elemento. Utilice un dispositivo con una cámara.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning();
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning();
+        }
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            return
+        }
+        
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if supportedBarCodes.contains(metadataObj.type) {
+            let barCodeObject = previewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+                found(code: metadataObj.stringValue);
+            }
+
+        }
+        
+//        //captureSession.stopRunning()
+//        
+//        if let metadataObject = metadataObjects.first {
+//            
+//            let readableObject = metadataObject as! AVMetadataMachineReadableCodeObject;
+//            qrCodeFrameView?.frame = readableObject.bounds
+//            
+////            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+////            found(code: readableObject.stringValue);
+//        }
+    }
+    
+    func found(code: String) {
+        
+        delegate?.transferir(texto: code)
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+}
+
